5e5bf4fca6301ace28b1a402dcabba427abea670
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2025-05-12 00:18:01 +0530

    Handle model and urls in Django Completed

--- chai/models.py
+++ chai/models.py
@@ -15,6 +15,8 @@ class ChaiVarity(models.Model):
     image = models.ImageField(upload_to='chais/')
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES, default='PL')
+    description = models.TextField(default='')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=10.00)
 
     def __str__(self):
         return self.name
